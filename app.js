de1ec6a840d00a99c763e26a1a08df2f9b3eb3ad
rawisha <rawvsmaw22@gmail.com>
2021-11-24 13:20:47 +0100

    fixed bugs

--- app.js
+++ app.js
@@ -7,18 +7,12 @@ const year = document.querySelectorAll(".year");
 const infos = [];
 const characters = [];
 
-/* Boolens for data and characers if loaded or not*/
-let dataLoaded = true;
-let charactersLoaded = true;
-
 /* Fetching filmUrl and converting them to json
 // Looping over each Movies and pushing the data into Infos above
 */
 fetch(filmUrl)
   .then((res) => res.json())
   .then((data) => {
-    dataLoaded = false;
-
     data.results.forEach((info) => {
       infos.push(info);
     });
@@ -37,15 +31,19 @@ fetch(filmUrl)
         fetch(url)
           .then((res) => res.json())
           .catch((error) => {
-            console.log("Something went terrabily wrong...");
+            console.log(error);
           })
       );
       /*
       // Grabbing all the promises data and pushing them into character arrays we put up above
+      // also removing the noClick class from section wrapper after character information has finished loading.
       */
       const setChars = () => {
         Promise.all(promises).then((res) => {
           characters.push(res);
+          document
+            .querySelector(".section-wrapper")
+            .classList.remove("noClick");
         });
       };
       // Running the setChars function
@@ -66,8 +64,6 @@ const showStaff = (valueOne) => {
   // once the value is above 0, then renderes out the data in the modal window
   */
   if (characters.length > 0) {
-    charactersLoaded = false;
-    document.querySelector("#name").classList.remove("loader");
     // Creating p elements for each data looped over characters array we put up above
     // rendering out each information to the modal window with the help of index (Value one)
     const characterNamesAsHTML = characters[valueOne]
@@ -77,7 +73,6 @@ const showStaff = (valueOne) => {
     document.querySelector("#titles").innerHTML = infos[valueOne].title;
   }
 };
-
 //function for opening the about modal
 const openAboutModal = () => {
   //get the buttons in the about cards, every button with the classname are collected in an array
@@ -93,7 +88,6 @@ const openAboutModal = () => {
     });
   }
 };
-
 //function for closing the about modal - starts with an onclick in the html-doc
 const closeAboutModal = () => {
   //hide the modal-div
